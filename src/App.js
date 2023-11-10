fc1d3585a850a827d2edaee945b363c576867a16
lumiereJE <lumiere_remplir@naver.com>
2023-11-10 12:53:23 +0900

    11/10 공동깃 합침 + 리뷰 스크롤 이벤트

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Store from './pages/store/Store';
-import Reserve from './pages/store/Reserve';
+import Store from './pages/finished-pages/store/Store';
+import Reserve from './pages/finished-pages/store/Reserve';
 
 
 function App() {
   return (
     <>
    {/* 추후 Router(페이지연결)이 이루어질 곳 */}
-      {/* <Store/> */}
-      <Reserve/>
+      <Store/>
+      {/* <Reserve/> */}
     </>
     
 
